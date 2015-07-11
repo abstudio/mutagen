d95936337530eb4ed9ebdf14173a2a1398bf91a8
Morulus <vladimirmorulus@gmail.com>
2015-07-12 00:27:30 +0300

    pseudoSelectors moves to querySelector, up to querySelector 0.0.4

--- src/mutagen.js
+++ src/mutagen.js
@@ -18,52 +18,8 @@ QuerySelector polyfill
 		String.prototype.mutate = factory();
 	}
 })
-("subzero", ["polyvitamins~querySelector@0.0.3"], function(extendedQuerySelector) {
-	
-	var regPseudoClasssDt = /:(eq|nth\-child)\(([0-9n\+ ]+)\)/ig,
-	queryExpr = /<([a-zA-Z0-9_]+) \/>/i,
-	argsExpr = /\[([a-zA-Z0-9_\-]*)[ ]?=([ ]?[^\]]*)\]/i;
-	
-		/*
-		Выборка элементов соответствующих псевдо-селектору
-		*/
-		var psopi = {
-			"eq": function(elements, attrs) {
-				var index;
-				if (!isNaN(index = parseInt(attrs[2]))) {
-					return [(index<0 && index*-1<elements.length) ? elements[elements.length-index] :
-					(index<elements.length ? elements[index] : [])];
-				} else {
-					return [];
-				}
-			},
-			"nth-child": function(elements, attrs) {
-
-				var n=false,i=0,rec=[],index;
-				if (!isNaN(index = parseInt(attrs[2]))) {
-
-					n=attrs[2].indexOf('n')>=0;
-					for (;i<elements.length;i++) {
-						if (!n&&i===index) rec.push(elements[i]);
-						if (n&&i%index===0) rec.push(elements[i]);
-					}
-				} else {
-
-					return [];
-				}
-				return rec;
-			}
-		}
-		var pseusoSelect = function(elements, selector) {
-			var psop = regPseudoClasssDt.exec(selector);
-
-			if ("function"!==typeof psopi[psop[1]]) {
-				return [];
-			} else {
-				return psopi[psop[1]](elements, psop);
-			}
-		},
-		each = function(subject, fn) {
+("subzero", ["polyvitamins~querySelector@0.0.4"], function(extendedQuerySelector) {
+		var each = function(subject, fn) {
 			for (var prop in subject) {
 				if (subject.hasOwnProperty(prop)) {
 					fn.call(subject, subject[prop], prop);
