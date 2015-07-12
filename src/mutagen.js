218d669ea707c807be9a9974410d1ca9054cfcb9
Morulus <vladimirmorulus@gmail.com>
2015-07-12 17:42:29 +0300

    adds postProcessor, mover preProcessor to beginnin of function, up to 0.1.9

--- src/mutagen.js
+++ src/mutagen.js
@@ -28,40 +28,33 @@ QuerySelector polyfill
 		},
 		regPlaceholder = /\{\{([^\} \.]+)([\.a-zA-Z0-9_]*)\}\}/gi;
 
-		var Mutagen = function(htmlElement, preProcessor) {
-			/*
-			Search for all placeholders
-			*/
-			var template = this,
-			matches = template.match(regPlaceholder),
-			replacings = {};
-			
-			matches.forEach(function(dph) {
-				regPlaceholder.lastIndex = 0;
-				var placeholderData = regPlaceholder.exec(dph),
-				placeholder = placeholderData[1],
-				keyname = placeholderData[2]!==""?placeholderData[2].substr(1) : 'innerHTML';
-				
-				if ("undefined"!==typeof replacings[placeholder]) return true;
-				replacings[dph] = '';
-
-				if (placeholder==='content') {
-					replacings[dph] = htmlElement.innerHTML;
-				} else {
-					var elements = extendedQuerySelector(placeholder, htmlElement);
-					if (elements) {
-						replacings[dph] = "undefined"!==typeof elements[0] ? elements[0][keyname] : "";
-					}
-				}
-			});
-			if ("function"===typeof preProcessor) preProcessor.call(htmlElement, replacings);
-
-			each(replacings, function(content, ph) {
-				template = template.replace(ph, content);
-			});
-			htmlElement.innerHTML = template;
-			return template;
-		}
+		var Mutagen = function(htmlElement, preProcessor, postProcessor) {
+             var template = this, matches = template.match(regPlaceholder), replacings = {};
+            if ("function" === typeof preProcessor) preProcessor.call(htmlElement, replacings);
+            
+            if (matches!==null)
+            matches.forEach(function(dph) {
+                regPlaceholder.lastIndex = 0;
+                var placeholderData = regPlaceholder.exec(dph), placeholder = placeholderData[1], keyname = placeholderData[2] !== "" ? placeholderData[2].substr(1) : "innerHTML";
+                if ("undefined" !== typeof replacings[placeholder]) return true;
+                replacings[dph] = replacings[dph] || "";
+                if (placeholder === "content") {
+                    replacings[dph] = htmlElement.innerHTML;
+                } else {
+                    var elements = extendedQuerySelector(placeholder, htmlElement);
+                    if (elements) {
+                        replacings[dph] = "undefined" !== typeof elements[0] ? elements[0][keyname] : replacings[dph] || "";
+                    }
+                }
+            });
+            
+            each(replacings, function(content, ph) {
+                template = template.replace(ph, content);
+            });
+            if ("function" === typeof postProcessor) postProcessor.call(htmlElement, template);
+            htmlElement.innerHTML = template;
+            return template;
+        };
 
 		Mutagen.query = extendedQuerySelector;
 
